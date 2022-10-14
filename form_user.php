2290b7b0a25eb31fbb16b231b44a80b445ce39cb
voyen057 <voyen057@gmail.com>
2022-10-14 08:07:55 +0700

    commit

--- form_user.php
+++ form_user.php
@@ -9,53 +9,72 @@ $_id = NULL;
 
 if (!empty($_GET['id'])) {
     $_id = $_GET['id'];
-    $user = $userModel->findUserById($_id);//Update existing user
+    $user = $userModel->findUserById($_id); //Update existing user
 }
 
 
 if (!empty($_POST['submit'])) {
 
+    foreach ($_POST as $item) {
+        if (preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $item)) {
+    echo "Special character found.";
+    die();
+    }
+    }
     if (!empty($_id)) {
-        $userModel->updateUser($_POST);
+    $userModel->updateUser($_POST);
     } else {
-        $userModel->insertUser($_POST);
+    $userModel->insertUser($_POST);
     }
     header('location: list_users.php');
-}
+    }
 
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>User form</title>
-    <?php include 'views/meta.php' ?>
-</head>
-<body>
-    <?php include 'views/header.php'?>
-    <div class="container">
+    ?>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <title>User form</title>
+        <?php include 'views/meta.php' ?>
+    </head>
+
+    <body>
+        <?php include 'views/header.php' ?>
+        <div class="container">
 
             <?php if ($user || !isset($_id)) { ?>
-                <div class="alert alert-warning" role="alert">
-                    User form
+            <div class="alert alert-warning" role="alert">
+                User form
+            </div>
+            <form name="myForm" method="POST" onsubmit="return validateForm()">
+                <input type="hidden" name="id" value="<?php echo $_id ?>">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input class="form-control" name="name" placeholder="Name"
+                        value='<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>'>
                 </div>
-                <form method="POST">
-                    <input type="hidden" name="id" value="<?php echo $_id ?>">
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <input class="form-control" name="name" placeholder="Name" value='<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>'>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" class="form-control" placeholder="Password">
-                    </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control" placeholder="Password">
+                </div>
 
-                    <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
-                </form>
+                <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
+            </form>
             <?php } else { ?>
-                <div class="alert alert-success" role="alert">
-                    User not found!
-                </div>
+            <div class="alert alert-success" role="alert">
+                User not found!
+            </div>
             <?php } ?>
-    </div>
-</body>
-</html>
+        </div>
+    </body>
+    <script>
+    function validateForm() {
+        var x = document.forms["myForm"]["name"].value;
+        if (/[\'^£$%&*()}{@#~?><>,|=_+¬-]/.test(x) == true) {
+            alert('Not allow special character')
+            return false;
+        }
+    }
+    </script>
+
+    </html>
